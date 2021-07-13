aa3055d98d5064fdf70225b8af255185a4fa7390
agimenes1975 <angelogimenes@yahoo.com.br>
2021-07-13 19:33:48 -0300

    Aula Completa

--- src/entities/Post.java
+++ src/entities/Post.java
@@ -1,10 +1,13 @@
 package entities;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class Post {
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 	
 	private Date moment;
 	private String title;
@@ -49,7 +52,7 @@ public class Post {
 	}
 
 	public Integer getLikes() {
-		return likes;
+		return likes;	
 	}
 
 	public void setLikes(Integer likes) {
@@ -67,6 +70,20 @@ public class Post {
 		comments.remove(comment);
 	}
 	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(title + "\n");
+		sb.append(likes );
+		sb.append(" Likes - ");
+		sb.append(sdf.format(moment)+ "\n");
+		sb.append(content + "\n");
+		sb.append("Comments: \n");
+		for (Comment c : comments) {
+			sb.append(c.getText() + "\n");
+		}
+		return sb.toString();
+		
+	}
 
 	
 	
